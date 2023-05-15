684e9e8829b4808d4ea3f306fbf3c8ceae825c5b
CarlosGD17 <88990623+CarlosGD17@users.noreply.github.com>
2023-05-14 23:41:54 -0600

    Update SubarrayDivision.py
    Code reduction

--- SubarrayDivision.py
+++ SubarrayDivision.py
@@ -2,10 +2,7 @@ def birthday(s, d, m):
     # Write your code here
     count = 0
     for i in range(len(s) - m + 1):
-        sum = 0
-        for j in range(m):
-            sum += s[i + j]
-        if sum == d:
+        if sum(s[i:i+m]) == d:
             count += 1
     return count
 
